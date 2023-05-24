fa8a11d26ccb0193b627863354fb95b4b1668543
Deepa Krishnan <deepakrishnan@deepas-mbp.lan>
2023-05-24 08:17:03 -0700

    Adding image to pane

--- NASAImageSearch/src/main/java/com/example/nasaimagesearch/ImageSearchApplication.java
+++ NASAImageSearch/src/main/java/com/example/nasaimagesearch/ImageSearchApplication.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ImageSearchApplication extends Application {
     @Override
@@ -38,18 +45,40 @@ public class ImageSearchApplication extends Application {
                         String title = rowData.getTitle();
                         String description = rowData.getDescription();
                         String dateCreated = rowData.getDateCreated();
-                        System.out.println("Double click on: " + rowData.getLink());
+
+                        //InputStream stream = new FileInputStream(link);
+                        Image image = new Image(link, true);
+                        ImageView imageView = new ImageView();
+                        imageView.setImage(image);
+                        imageView.setX(10);
+                        imageView.setY(10);
+                        imageView.setFitWidth(575);
+                        imageView.setPreserveRatio(true);
+
                         final Stage dialog = new Stage();
-                        dialog.initModality(Modality.APPLICATION_MODAL);
                         dialog.initOwner(stage);
-                        VBox dialogBox = new VBox(200);
-                        dialogBox.getChildren().add(new Text("Link is" + link));
-                        dialogBox.getChildren().add(new Text("Title is" + title));
-                        dialogBox.getChildren().add(new Text("Description is" + description));
-                        dialogBox.getChildren().add(new Text("Date Created is" + dateCreated));
-                        Scene dialogScene = new Scene(dialogBox);
+                        Group root = new Group(imageView);
+                        //StackPane pane = new StackPane();
+
+                        //pane.getChildren().add(new Text("Title is " + title));
+                        //pane.getChildren().add(new Text("Description is " + description));
+                        //pane.getChildren().add(new Text("Date Created is " + dateCreated));
+                        //pane.getChildren().add(imageView);
+                        Scene dialogScene = new Scene(root, 595, 370);
                         dialog.setScene(dialogScene);
                         dialog.show();
+
+                        //final Stage dialog = new Stage();
+                        //dialog.initModality(Modality.APPLICATION_MODAL);
+                        //dialog.initOwner(stage);
+                        //VBox dialogBox = new VBox(200);
+                        //dialogBox.getChildren().add(new Text("Link is" + link));
+                        //dialogBox.getChildren().add(new Text("Title is" + title));
+                        //dialogBox.getChildren().add(new Text("Description is" + description));
+                        //dialogBox.getChildren().add(new Text("Date Created is" + dateCreated));
+                        //Scene dialogScene = new Scene(dialogBox);
+                        //dialog.setScene(dialogScene);
+                        //dialog.show();
                     }
                 }
             });
